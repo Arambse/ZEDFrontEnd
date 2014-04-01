1d6733e9c9a2d4c1b50b711a43c48b73859ac702
Aram Ben Shushan Ehrlich <arambse@gmail.com>
2014-04-01 16:17:55 +0300

    times select and options

--- app/scripts/controllers/partials/times.js
+++ app/scripts/controllers/partials/times.js
@@ -1,19 +1,17 @@
 angular.module('ZedApp')
-  .controller('TimesCtrl', ['$scope', '$state', '$q', '$http', 'Events', 'User',
-    function ($scope, $state, $q, $http, Events, User) {
-    
-      //Time picker
-      jQuery('#time-picker').timepicker({
-         defaultTime: 'current',
-        minuteStep: 15,
-        showMeridian: false,
-        showWidget: true,
-        disableFocus: true,
-        template: 'dropdown'
-      });
+  .controller('TimesCtrl', ['$scope', '$state', '$q', '$http', 'Events', 'Shifts',
+    function ($scope, $state, $q, $http, Events, Shifts) {
+
+      $scope.shifts = Shifts;
+      $scope.currentShift = Shifts['Morning'];
 
       //Date Picker
       jQuery('#date-picker').datepicker();
+
+      //Debugging
+      $scope.shiftChanged = function() {
+        console.log($scope.currentTime);
+      }
 
 }]);
 
